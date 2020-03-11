ce34f1e00af7545aa5df42293b576ae9e1ed3412
ChrisZie <chrissie2788@gmail.com>
2020-03-11 14:28:23 +0800

    rebuild docs

---
+++ docs/js/custom.js
@@ -0,0 +1,35 @@
+$(".contactcard-toggle").on("click", function(){
+	
+	// Card toggle state 	
+	$(".contactcard-toggle").removeClass("active");
+	$(this).addClass("active");
+	
+	var isAnimating = false;
+	
+	if( !isAnimating ){
+		isAnimating = true;
+		
+		$(".contactcard").find(".contactcard-content").css("z-index",0);
+		$(".contactcard").removeClass("active");
+
+		var that = $(this);
+
+		$(this).siblings().css("z-index",1);
+
+		setTimeout(function(){
+			that.parent().toggleClass("active").find(".card-content").on("transitionend", function(){
+				isAnimating = false;
+			});	;
+			
+		},10);
+	} else {
+		return;
+	}
+});
+
+$(".serviceSelect").on("click", function(){
+	
+	// Card toggle state 	
+	$(this).toggleClass("active");
+	
+});

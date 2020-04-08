a3c0f239eec9c27046f817136f6c4ebf21584525
ChrisZie <chrissie2788@gmail.com>
2020-04-08 15:27:03 +0800

    rebuild docs content

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,10 @@ rm -rf docs
 cd ..
 rm -rf LIVE-WS/*
 cd LIVE-WS/
-git add . 
-git commit -m "rebuild delete content"
-git pull
+
 cd ..
 cd symphony/
 hugo --minify
 cp -R docs/ ../LIVE-WS
 git add . 
 git commit -m "rebuild docs content"
-git pull
